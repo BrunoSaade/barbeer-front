e3d1408d3fdb7867a3d26754ef0643ae396a8350
BrunoSaade <brsaade@gmail.com>
2023-03-23 19:51:36 -0300

    Feat: VueRouter added

--- src/main.js
+++ src/main.js
@@ -4,22 +4,27 @@ import routes from './routes'
 import './assets/main.css'
 import store from './store'
 
+import { createRouter, createWebHistory } from 'vue-router';
+
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 /* import specific icons */
 import { faUserSecret, faUser, faLock } from '@fortawesome/free-solid-svg-icons'
-
 /* add icons to the library */
 library.add(faUserSecret, faUser, faLock)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
 
 
 createApp(App)
     .use(store)
-    .use(routes)
+    .use(router)
     .component('icon', FontAwesomeIcon)
     .mount('#app')
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import Home from './views/Home.vue';
+import Register from './views/Register.vue';
 export default [
     {
         path: '/',
         component: Home,
-    }
+    },
+    {
+        path: '/register',
+        component: Register,
+    },
 ]
