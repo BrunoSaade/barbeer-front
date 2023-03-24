859e0e6ea3271da70106c487c6601fde4854254a
BrunoSaade <brsaade@gmail.com>
2023-03-24 15:16:30 -0300

    Feat: Color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
+  
   theme: {
     extend: {
       colors: {
         blue: ["#6CE1F4"],
         primary: ["#17241D"],
         grey: ['#FFFFFFBF', '#000000E5'],
-        orange: ['#AF5118'],
+        orange: ['#AF5118', '#AF7240'],
       }
     },
   },
