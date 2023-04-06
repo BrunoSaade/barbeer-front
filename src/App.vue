58b165f5839a0b8e4430de8522073ecc50733b87
Bruno-Saade <brsaade@gmail.com>
2023-04-06 20:05:17 -0300

    Fix: Icon changed for App route to apply in another logged pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
 #app.app-container
   Navbar(v-if="isLogged")
+  .app-container-logged--header(v-if="isLogged")
+    icon.app-container-logged--header-icon(
+      icon='fa-solid fa-bars'
+      @click="handleMenuMobileOpen"
+    )
   router-view
 </template>
 <script>
@@ -20,13 +25,26 @@ export default{
       ]),
       isLogged() {
         return this.getIsLogged
+      },
+  },
+  methods: {
+    ...mapActions([
+      'setIsMenuMobileOpen'
+    ]),
+    handleMenuMobileOpen() {
+        this.setIsMenuMobileOpen(true)
       }
   },
-  methods: {},
 }
 </script>
 <style scoped>
   .text-black{
     color: rgb(113, 42, 42);
+  }
+  .app-container-logged--header {
+    @apply w-full z-30 fixed top-0 left-0 right-0 p-7;
+  }
+  .app-container-logged--header-icon {
+    @apply text-4xl text-grey-0;
   }
 </style>
--- src/views/logged/Main.vue
+++ src/views/logged/Main.vue
@@ -1,11 +1,6 @@
 <template lang="pug">
 VContainer.main
   VContainer.main--sec-container
-    .main--header 
-      icon.main--header-icon(
-        icon='fa-solid fa-bars'
-        @click="handleMenuMobileOpen"
-      )
 </template>
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex'
@@ -40,12 +35,6 @@ export default{
   }
   .main--sec-container {
     @apply md:flex md:items-center md:justify-around max-w-[400px] md:max-w-[2008px];
-  }
-  .main--header {
-    @apply w-full z-30 fixed top-0 left-0 right-0 p-7;
-  }
-  .main--header-icon {
-    @apply text-4xl text-grey-0;
   }
 </style>
   
