65f682e6b7fa34dda679676f56e826c6e8801d93
Bruno-Saade <brsaade@gmail.com>
2023-05-25 19:45:58 -0300

    Feat: Added new comps

--- src/views/logged/Main.vue
+++ src/views/logged/Main.vue
@@ -2,12 +2,16 @@
 VContainer.main
   VContainer.main--sec-container
     VDatePicker 
+    VTurnPicker.mt-4
+    VTimePicker.mt-4
 </template>
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex'
 
 import VContainer from '../../global_components/VContainer.vue'
 import VDatePicker from '../../global_components/VDatePicker.vue'
+import VTimePicker from '../../global_components/VTimePicker.vue'
+import VTurnPicker from '../../global_components/VTurnPicker.vue'
 
 export default{
   data() {
@@ -25,6 +29,8 @@ export default{
   components: {
     VContainer,
     VDatePicker,
+    VTimePicker,
+    VTurnPicker
   }
 }
 </script>
@@ -33,7 +39,7 @@ export default{
     @apply bg-primary-0 h-screen max-w-full place-content-center flex md:items-center md:justify-around pt-32;
   }
   .main--sec-container {
-    @apply md:flex md:items-center md:justify-around max-w-[400px] md:max-w-[2008px];
+    @apply md:flex md:items-center md:justify-around max-w-[360px] md:max-w-[2008px];
   }
 </style>
   
