7d0793910fdcd418eef80496eb4b1b8d00925664
Bruno-Saade <brsaade@gmail.com>
2023-06-23 00:01:06 -0300

    Feat: Created booking file

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Home from './views/Home.vue';
 import Register from './views/Register.vue';
 import Main from './views/logged/Main.vue';
 import Profile from './views/logged/Profile.vue';
+import Booking from './views/logged/Booking.vue';
 
 export default [
     {
@@ -19,5 +20,9 @@ export default [
     {
         path: '/logged/profile',
         component: Profile,
+    },
+    {
+        path: '/logged/booking',
+        component: Booking,
     },
 ]
---
+++ src/views/logged/Booking.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+VContainer.booking
+  VContainer.booking--sec-container
+</template>
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+import VContainer from '../../global_components/VContainer.vue'
+
+export default{
+  data() {
+      return {};
+  },
+  mounted() {
+    this.setIsLogged(true)
+  },
+  computed: {},
+  methods: {
+    ...mapActions([
+      'setIsLogged',
+    ]),
+  },
+  components: {
+    VContainer
+  }
+}
+</script>
+<style scoped>
+.booking {
+    @apply bg-primary-0 h-screen max-w-full place-content-center flex md:items-center md:justify-around;
+  }
+  .booking--sec-container {
+    @apply md:flex md:items-center md:justify-around max-w-[400px] md:max-w-[2008px];
+  }
+</style>
+  
+
+
